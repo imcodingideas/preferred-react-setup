958c1cabdfc7988e07e28c8f8be07a263e24f501
Joseph Chambers <joseph@michael-chambers.com>
2020-07-11 10:55:01 -0700

    Fix linting issues.

--- webpack/util.buildIdentifier.js
+++ webpack/util.buildIdentifier.js
@@ -1,6 +1,9 @@
-const glob = require('../package.json')
-const exec = require('child_process').execSync
+const glob = require('../package.json');
+const exec = require('child_process').execSync;
 
-const gitHash = exec('git rev-parse HEAD', { encoding: 'utf-8' }).substring(0, 8)
+const gitHash = exec('git rev-parse HEAD', { encoding: 'utf-8' }).substring(
+  0,
+  8
+);
 
-module.exports = `${glob.version}-${gitHash}`
+module.exports = `${glob.version}-${gitHash}`;
