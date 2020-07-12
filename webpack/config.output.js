ed3bbe5fd691886dae2cc191a93735b3772ce20b
Joseph Chambers <joseph@michael-chambers.com>
2020-07-12 10:07:33 -0700

    Remove CDN folder from output

--- webpack/config.output.js
+++ webpack/config.output.js
@@ -2,13 +2,13 @@ const path = require('path');
 const buildIdentifier = require('./util.buildIdentifier');
 const isProduction = require('./util.isProduction');
 
-const output = env => ({
-  filename: `app/${buildIdentifier}/${fileChunkName(env)}`,
-  path: path.resolve(__dirname, `./../build/${env.TARGET || 'dev'}/cdn`),
-  publicPath: `/`,
-});
-
 const fileChunkName = env =>
   isProduction(env) ? '[name].[hash].js' : '[name].[hash].js';
+
+const output = env => ({
+  filename: `app/${buildIdentifier}/${fileChunkName(env)}`,
+  path: path.resolve(__dirname, `./../build/${env.TARGET || 'dev'}`),
+  publicPath: `/`,
+});
 
 module.exports = output;
